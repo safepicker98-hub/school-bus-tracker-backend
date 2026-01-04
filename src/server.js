4acbb1a06366ee05619ee32cbe558c1b2b790895
GowthamSelvaraj123 <gowthamdesigner94@gmail.com>
2026-01-04 15:46:05 +0530

    Initial backend commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const client = require('./config/db');
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+client;
+
+app.listen(3000, () => 
+    console.log("Server is starting..."))
